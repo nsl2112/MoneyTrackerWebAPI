ffc7b081580d6851ca9f63e5fd0d3b9e33cd4145
sonlam <sonlamnguyen2112@gmail.com>
2026-06-15 22:15:20 +0700

    First commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+try
+{
+    Log.Information("Starting MoneyTracker API...");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+    builder.Services.AddSerilog((servies, lc) =>
+    {   
+        lc.ReadFrom.Configuration(builder.Configuration)
+          .ReadFrom.Services(servies)
+          .Enrich.FromLogContext()
+          .WriteTo.Console();
+    });
+    builder.Services.AddControllers();
+    // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+    builder.Services.AddOpenApi();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
+}
+catch (Exception ex)
+{
+    Log.Fatal("An unhandled exception occurred: {ExceptionMessage}", ex.Message);
+}
+finally
+{
+    Log.CloseAndFlush();
+}
